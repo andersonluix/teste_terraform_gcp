770458c771cb5c475844fa3c1b69a7102aa88e62
anderson <anderson@teste.com>
2022-11-26 17:05:54 -0200

    cria var env

--- IaC/vpc.tf
+++ IaC/vpc.tf
@@ -7,6 +7,6 @@ provider "google" {
 
 # VPC
 resource "google_compute_network" "vpc2" {
-  name                    = "${{ secrets.VPCNAME }}"
+  name                    = "$vpcname"
   auto_create_subnetworks = "false"
 }
