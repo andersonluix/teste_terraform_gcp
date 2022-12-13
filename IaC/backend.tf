ebab6ee8f22522267a6ec279486c03774b385b6a
Anderson Luiz <andersonlbsilva@gmail.com>
2022-12-13 17:38:42 -0300

    atualiza backend2

--- IaC/backend.tf
+++ IaC/backend.tf
@@ -7,20 +7,3 @@ terraform {
     prefix  = "terraform/state"
   }
 }
-
-
-data "terraform_remote_state" "foo" {
-  backend = "gcs"
-  config = {
-    bucket  = "terraform-state"
-    prefix  = "prod"
-  }
-}
-
-resource "template_file" "bar" {
-  template = "${greeting}"
-
-  vars {
-    greeting = "${data.terraform_remote_state.foo.greeting}"
-  }
-}
